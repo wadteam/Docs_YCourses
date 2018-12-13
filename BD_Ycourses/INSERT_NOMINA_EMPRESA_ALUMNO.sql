175428feb5bef413d859362f4f30bee264a482a0
Laptop edo <gstv.qnd@outlook.com>
2018-12-12 23:10:21 -0300

    Añadiendo NOMINA_EMPRESA_ALUMNO, SERVICIO y TIPO_SERVICIO
    Tanda de nuevos cambios

---
+++ BD_Ycourses/INSERT_NOMINA_EMPRESA_ALUMNO.sql
@@ -0,0 +1,24 @@
+USE bd_ycourses;
+/*INSERTANDO (20) FILA(S)*/
+INSERT INTO nomina_empresa_alumno (tipoalumno, empresa_rutempresa, alumno_idalumno, estado)
+VALUES  
+  ('Beneficiado', '1', 1, 'Incluido'),
+  ('Beneficiado', '1', 2, 'Incluido'),
+  ('Beneficiado', '1', 3, 'Incluido'),
+  ('Beneficiado', '1', 4, 'Incluido'),
+  ('Beneficiado', '1', 5, 'Incluido'),
+  ('Beneficiado', '1', 6, 'Incluido'),
+  ('Beneficiado', '1', 7, 'Incluido'),
+  ('Beneficiado', '1', 8, 'Incluido'),
+  ('Beneficiado', '1', 9, 'Incluido'),
+  ('Beneficiado', '1', 10, 'Excluido'),
+  ('Beneficiado', '2', 11, 'Excluido'),
+  ('Beneficiado', '2', 12, 'Excluido'),
+  ('Beneficiado', '2', 13, 'Excluido'),
+  ('Beneficiado', '2', 14, 'Excluido'),
+  ('Beneficiado', '2', 15, 'Excluido'),
+  ('Beneficiado', '2', 16, 'Excluido'),
+  ('Beneficiado', '2', 17, 'Excluido'),
+  ('Beneficiado', '3', 18, 'Incluido'),
+  ('Beneficiado', '3', 19, 'Incluido'),
+  ('Beneficiado', '3', 20, 'Incluido');
---
+++ BD_Ycourses/INSERT_SERVICIO.sql
@@ -0,0 +1,24 @@
+USE bd_ycourses;
+/*INSERTANDO (20) FILA(S)*/
+INSERT INTO servicio (nombreservicio, descripcion, fechainicio, fechatermino, valorreferencial, direccionlugar, estado, tipo_servicio_idtipo_servicio, relator_rutrelator, ciudad_idciudad) 
+VALUES
+  ('Curso de Electrónica Magistral', 'Curso personalizado', '2018-03-04', '2018-01-07', 41316, '52620 Dwight Terrace', 'Por publicar', 7, '16944870-1', 6),
+  ('Curso de Mecánica Pesada Online', 'Curso personalizado', '2018-06-28', '2018-03-06', 348085, '3895 Nelson Junction', 'Publicado', 12, '12345789-7', 8),
+  ('Cisco Networking Academy', 'Capacitación personalizada', '2018-07-01', '2018-03-04', 382936, '3 7th Road', 'Publicado', 2, '12345789-7', 2),
+  ('Negocios y Marketing Digital', 'Seminario personalizado', '2018-09-21', '2018-01-13', 28304, '9 Magdeline Crossing', 'Por publicar', 5, '12345781-1', 9),
+  ('Electrónica y prototipado complejo', 'Curso personalizado', '2018-09-09', '2018-01-07', 56768, '2014 Donald Parkway', 'Publicado', 7, '12345788-9', 1),
+  ('Amazon Web Services Essentials', 'Capacitación', '2018-10-01', '2018-11-05', 391443, '55768 Melvin Plaza', 'Publicado', 8, '12345788-9', 1),
+  ('La importancia del deporte en el trabajo', 'Seminario personalizado', '2018-05-10', '2018-10-17', 355270, '720 Texas Place', 'Por publicar', 9, '12345788-9', 5),
+  ('Manipulación de Alimentos en Minería', 'Curso personalizado', '2018-07-19', '2018-12-16', 295891, '96 Rutledge Avenue', 'Publicado', 10, '12345781-1', 3),
+  ('Cloud Computing Engine en Empresas', 'Seminario personalizado', '2018-04-16', '2018-12-03', 89702, '79431 Claremont Trail', 'Por publicar', 9, '12345777-3', 8),
+  ('Servidores Web S2', 'Seminario personalizado', '2018-06-25', '2018-12-23', 291996, '6 Ryan Trail', 'Por publicar', 9, '12345781-1', 7),
+  ('Contabilidad Moderna y Rápida', 'Seminario personalizado', '2017-12-18', '2018-12-28', 350567, '0 Talmadge Center', 'Publicado', 5, '12345777-3', 1),
+  ('Cisco Senior Academy', 'Capacitación personalizada', '2018-03-27', '2018-05-23', 222688, '6 Dottie Alley', 'Por publicar', 2, '12345771-4', 2),
+  ('El Bushido en las corporaciones', 'Seminario personalizado', '2018-03-10', '2018-08-08', 94868, '1 Cottonwood Center', 'Por publicar', 9, '16944870-1', 3),
+  ('Introducción a las Tec.Web', 'Capacitación personalizada', '2018-04-06', '2018-09-25', 401983, '0335 Thierer Avenue', 'Por publicar', 8, '16944870-1', 10),
+  ('Dominando las Tec.Web', 'Capacitación personalizada', '2018-02-03', '2018-11-10', 172588, '7 Jay Avenue', 'Publicado', 8, '12345781-1', 5),
+  ('Certificación oficial Tec.Web', 'Capacitación personalizada', '2018-05-31', '2018-07-10', 144736, '2 Troy Avenue', 'Publicado', 8, '16944870-1', 5),
+  ('Cisco Packet Tracer for Bussiness', 'Curso personalizado', '2018-06-15', '2018-04-17', 81351, '73496 Division Parkway', 'Publicado', 2, '12345771-4', 7),
+  ('Ciclo de Charlas 2019', 'Seminario Magistral', '2018-09-01', '2018-10-23', 237792, '75 Crowley Plaza', 'Publicado', 11, '12345788-9', 2),
+  ('I.A & Deep Learning', 'Capacitación Personalizada', '2018-09-23', '2018-05-24', 142802, '9 Fairview Center', 'Por publicar', 8, '12345788-9', 8),
+  ('Deportes corporativos 2019', 'Seminario personalizado', '2018-08-26', '2018-02-14', 171240, '395 Shopko Avenue', 'Publicado', 9, '12345781-1', 3);
